0477a941405d1312831f19c5cced403e0cac903a
alfacom <alfacom141@gmail.com>
2024-11-15 18:34:11 +0300

    Typo corrections 3 T_T

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,7 +3,7 @@ from random import randint
 
 from brain_games.games.main import play_game, YES, NO
 
-PRIME_RULE = f'Answer "{YES}" if the number is prime. Otherwise answer "{NO}".'
+PRIME_RULE = f'Answer "{YES}" if given number is prime. Otherwise answer "{NO}".'
 
 
 def is_prime(number: int) -> bool:
